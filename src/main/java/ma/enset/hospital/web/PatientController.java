627e7060dd93ccdd47fad5025c8015c17fed9f38
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-05-07 12:43:11 +0200

    partie2

--- src/main/java/ma/enset/hospital/web/PatientController.java
+++ src/main/java/ma/enset/hospital/web/PatientController.java
@@ -1,5 +1,6 @@
 package ma.enset.hospital.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.enset.hospital.entities.Patient;
 import ma.enset.hospital.repository.PatientRepository;
@@ -7,8 +8,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -43,4 +47,39 @@ public class PatientController {
     public String home(){
         return "redirect:/index"; // la vue
     }
+
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> listPatients(){
+        return patientRepository.findAll();
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient", new Patient());
+        return "formPatients"; // la vue
+    }
+
+    // insert | update
+    @PostMapping(path = "/save")
+    // @Valid => une fois que tu stocke les données dans patient tu fait la validation, stocke les erreur dans BindingResult
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0")  int page,
+                       @RequestParam(defaultValue = "")  String keyword){
+        if(bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient); // if id paitent == null => insert , else id !=null => update
+        //return "redirect:/formPatients"; // nouveau model
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model,Long id,String keyword, int page){
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if(patient==null) throw new RuntimeException("Patient introuvable");
+        model.addAttribute("patient", patient);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient"; // la vue
+    }
+
 }
